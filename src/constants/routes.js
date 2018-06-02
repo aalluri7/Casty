b92ca50cd710c7c1dd132bda65b37cb5f2090b52
Aditya Varma Alluri <aalluri7@gmail.com>
2018-06-02 12:27:01 -0700

    delete

--- src/constants/routes.js
+++
@@ -1,1 +0,0 @@
-export const Route = ["home", "search"];
--- src/utils/decodeHtml.js
+++
@@ -1,7 +0,0 @@
-function decodeHtml(html) {
-  var txt = document.createElement("textarea");
-  txt.innerHTML = html;
-  return txt.value;
-}
-
-export default decodeHtml;
