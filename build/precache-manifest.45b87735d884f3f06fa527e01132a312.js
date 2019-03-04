eb70907b0df1942e3209d00ae61e8344878aec4c
Aditya Alluri <aalluri7@gmail.com>
2019-03-04 13:47:37 -0800

    reset

---
+++ build/precache-manifest.45b87735d884f3f06fa527e01132a312.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Casty/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "31995aa125925e0c5539",
+    "url": "/Casty/static/js/runtime~main.31995aa1.js"
+  },
+  {
+    "revision": "ad3efb17c091fec45000",
+    "url": "/Casty/static/js/main.ad3efb17.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/Casty/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "ad3efb17c091fec45000",
+    "url": "/Casty/static/css/main.31daa69d.chunk.css"
+  },
+  {
+    "revision": "49bfad9a97aa0765f0e7f785e00b2ca6",
+    "url": "/Casty/index.html"
+  }
+];
---
+++ build/service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/Casty/precache-manifest.45b87735d884f3f06fa527e01132a312.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/Casty/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
